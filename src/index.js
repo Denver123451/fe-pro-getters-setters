3010a1669a231546308467064c548710aefbf5b2
Denver12345 <villaan12345>
2022-07-08 11:20:10 +0300

    corected!

--- src/index.js
+++ src/index.js
@@ -21,39 +21,28 @@ export const school = {
     7: new Student('Ivan', [76, 89, 78, 98, 98, 99, 89, 96]),
   },
   get aGradeStudents() {
-    const students = [];
-    Object.values(this.students).filter((item) => {
-      if (item.averageGrade >= 90) {
-        students.push(item.name);
-      }
-    });
-    return students.join(', ');
+    return Object.values(this.students)
+      .filter((item) => item.averageGrade >= 90)
+      .map(({ name }) => name)
+      .join(', ');
   },
   get bGradeStudents() {
-    const students = [];
-    Object.values(this.students).filter((item) => {
-      if (item.averageGrade >= 75 && item.averageGrade < 90) {
-        students.push(item.name);
-      }
-    });
-    return students.join(', ');
+    return Object.values(this.students)
+      .filter((item) => item.averageGrade >= 75 && item.averageGrade <= 89)
+      .map(({ name }) => name)
+      .join(', ');
   },
   get cGradeStudents() {
-    const students = [];
-    Object.values(this.students).filter((item) => {
-      if (item.averageGrade >= 60 && item.averageGrade < 75) {
-        students.push(item.name);
-      }
-    });
-    return students.join(', ');
+    return Object.values(this.students)
+      .filter((item) => item.averageGrade >= 60 && item.averageGrade <= 74)
+      .map(({ name }) => name)
+      .join(', ');
   },
+
   get dGradeStudents() {
-    const students = [];
-    Object.values(this.students).filter((item) => {
-      if (item.averageGrade >= 0 && item.averageGrade < 60) {
-        students.push(item.name);
-      }
-    });
-    return students.join(', ');
+    return Object.values(this.students)
+      .filter((item) => item.averageGrade >= 0 && item.averageGrade <= 59)
+      .map(({ name }) => name)
+      .join(', ');
   },
 };
